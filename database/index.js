da30cc6c846ab80487911f8836c70b8553d2e82c
William Casey <notwillcasey@gmail.com>
2021-09-30 22:13:53 -0600

    Handling information parsing and DB connections

--- database/index.js
+++ database/index.js
@@ -3,8 +3,8 @@ const pool = new Pool({
   user: process.env.pgUser,
   database: process.env.pgDatabase,
   password: process.env.pgPass,
-  // host: process.env.pgHost,
-  // port: process.env.pgPort,
+  host: process.env.pgHost,
+  port: process.env.pgPort,
   connectionTimeoutMillis: 10,
   allowExitOnIdle: true,
   idleTimeoutMillis: 10
@@ -21,50 +21,79 @@ const pool = new Pool({
 // }
 
 // create promise to retrun the results from querying DB for featuers and values -- also format features and values into needed format here
-const relatedFeaturesQuery = (relatedIds, masterID) => {
-  const allIds = masterID.concat(relatedIds).join()
-  console.log(allIds)
-  return client.query(`SELECT * FROM featuresFinal WHERE product_id IN (${allIds});`)
+const relatedFeaturesQuery = (relatedIds, masterID, client) => {
+  relatedIds.unshift(Number(masterID))
+  return client.query(`SELECT * FROM featuresFinal WHERE product_id IN (${relatedIds});`)
+};
+
+const separateFeatures = (features, values) => {
+  const featuresFinal = [];
+  const f = features.split(',');
+  const v = values.split(',');
+  f.map((feature, index) => {
+    const featureValuePair = {
+      feature: feature,
+      value: v[index]
+    }
+    featuresFinal.push(featureValuePair)
+  })
+  return featuresFinal;
 }
 
 module.exports.getRelatedIds = (req, res) => {
+  console.log(req.query.id)
   if (!req.query.id) {
     res.status(400).send('missing product ID');
     return;
   }
-  // let completeData = {
-  //   product_id: {
-  //     id: req.query.id,
-  //     features: [],
-  //     values: []
-  //   },
-  //   related_ids: [
-  //     {
+  let completeData = {};
+  let finalData = [];
 
-  //     }
-  //   ]
-  // }
   pool
     .connect()
     .then((client) => {
       return client.query(`SELECT related_ids FROM relatedFinal WHERE current_product_id=${req.query.id}`)
         .then(response => {
           // client.release();
-          let ids = response.rows[0].related_ids.split(',').map(Number)
-          const allIds = masterID.concat(relatedIds).join()
-          console.log(allIds)
-          return client.query(`SELECT * FROM featuresFinal WHERE product_id IN (${allIds});`)
-
+          let relatedIds = response.rows[0].related_ids.split(',').map(Number)
+          let allIds = relatedIds;
+          allIds.unshift(Number(req.query.id))
+          allIds.map((currentID) => {
+            const productInfo = {
+              id: currentID
+            }
+            completeData[currentID] = productInfo
+          })
+          const features = relatedFeaturesQuery(relatedIds, req.query.id, client)
           // return promise.all for three async functions created above to get all data needed
             // format data in final then block and send response with object
+          return Promise.all([features])
         })
-        .then((features) => {
+        // .then((allData) => {
+        //   //handle the data formatting here
+
+        // })
+        .then((allData) => {
           client.release();
-          console.log(features)
+          allData[0].rows.map((id) => {
+            let pid = id.product_id;
+            let product = completeData[pid];
+            const featuresValues = separateFeatures(id.features, id.values)
+            product.features = featuresValues
+          })
+          for (const key in completeData) {
+            if (key == req.query.id) {
+              continue;
+            }
+            finalData.push(completeData[key]);
+          }
+          finalData.push(completeData[req.query.id])
+          console.log(finalData)
           res.status(200)
-          res.send(features)
+          res.send(finalData)
         })
         .catch(err => {
+          console.log(err)
           client.release();
           res.status(400).send('Error finding ID')
         })
